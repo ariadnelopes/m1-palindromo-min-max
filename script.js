d87c3d476752f1e56b3eff32b03a575efe8ad42e
Ariadne Lopes <ariadne_als@outlook.com>
2024-02-28 14:07:56 -0300

    codar javascript palíndromo-min-max

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+    let palavraFrase = "";
+    let palavraFraseInvertida = "";
+    const removerEspacos = str.replace(/ /g,'').toLowerCase();
+    for( let i = 0; i < removerEspacos.length; i++) {
+        palavraFrase += removerEspacos[i]
+        palavraFraseInvertida = removerEspacos[i] + palavraFraseInvertida
+    }
+    return palavraFrase === palavraFraseInvertida
 }
 
+
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+    let array = []
+    let maiorNumero = 0;
+    let MenorNumero = 0;
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] > maiorNumero) {
+            maiorNumero = arr[i]
+        }
+        if(i === 0 || arr[i] < MenorNumero) {
+            MenorNumero = arr[i]
+        }
+    }
+    return array = [MenorNumero, maiorNumero]
 }
